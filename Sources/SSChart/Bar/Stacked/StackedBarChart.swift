43a90f2a14265bde99a357f7f8a6629b5d13f373
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2022-09-05 09:05:13 +0900

    Create StackedBarChart.swift
    #11

---
+++ Sources/SSChart/Bar/Stacked/StackedBarChart.swift
@@ -0,0 +1,29 @@
+//
+//  StackedBarChart.swift
+//  
+//
+//  Created by YJ on 2022/09/02.
+//
+
+import Foundation
+import UIKit
+
+public class StackedBarChart: UIView {
+    
+    
+    // MARK: - init
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - override
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+}
+
